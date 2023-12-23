78f8339743bed0c06cf6ebcfbd3914a636bd3ea6
subha20032007 <meunknown466@gmail.com>
2023-12-23 22:12:47 +0530

    Ad

---
+++ mantra_app/src/page/admin/Dashbord.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Dashbord = () => {
+  return (
+    <div>Dashbord</div>
+  )
+}
+//agf
--- mantra_app/src/page/users/AllRoutes.jsx
+++ mantra_app/src/page/users/AllRoutes.jsx
@@ -2,17 +2,13 @@ import {React, useState,useEffect} from 'react'
 import {Routes} from "react-router-dom"
 import { Route } from 'react-router-dom'
 import { Admin } from '../admin/Admin'
-<<<<<<< HEAD
-import { Login } from './Login'
-import { Home } from './Home'
 import { PrivateRoutes } from '../../componets/user/PrivateRoutes'
-=======
 import Login  from './Login'
 import Home  from './Home'
 import {auth} from '../../firebase/FirebaseConfig'
 import { onAuthStateChanged } from 'firebase/auth'
 import Register from './Register'
->>>>>>> dev-khurram
+
 export const AllRoutes = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -33,16 +29,15 @@ export const AllRoutes = () => {
   return (
     <div>
         <Routes>
-<<<<<<< HEAD
+
             <Route path="/" element={<PrivateRoutes><Home/></PrivateRoutes>}/>
             <Route path="/login" element={<Login/>} />
             <Route path="/Admin" element={<PrivateRoutes><Admin/></PrivateRoutes>}/>
-=======
             <Route path="/" element={isAuthenticated? <Home /> : <Login />} />
             <Route path="/login" element={<Login/>} />
             <Route path="/Admin" element={<Admin/>}/>
             <Route path='/register' element={<Register />} />
->>>>>>> dev-khurram
+
            <Route path="*" element={<h1>Page Not Found ...</h1>}/>
         </Routes>
     </div>
