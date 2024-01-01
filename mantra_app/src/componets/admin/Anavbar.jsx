7e5d4902324e84f36eb770c721562770503ea225
subha20032007 <meunknown466@gmail.com>
2024-01-01 23:44:13 +0530

    Ad

--- mantra_app/src/componets/admin/Anavbar.jsx
+++ mantra_app/src/componets/admin/Anavbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const aNavbar = () => {
   return (
-    <div>aNavbar</div>
+    <DIV>aNavbar</DIV>
   )
 }
+const DIV=styled.div`
+height:70px;
+display:flex;
+border:'1px solid black';
+justify-content: space-around;
+`
